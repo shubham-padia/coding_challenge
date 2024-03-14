2922e65c9caf1db89438ac04a67fdf420698f492
Shubham Padia <shubhamapadia@gmail.com>
2024-03-14 14:25:37 +0530

    Migration: Initial DB with relevant models.

---
+++ src/prisma/migrations/20240314084420_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateEnum
+CREATE TYPE "DAYS" AS ENUM ('SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY');
+
+-- CreateTable
+CREATE TABLE "Restaurants" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Restaurants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Dishes" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(6,2) NOT NULL,
+    "RestaurantId" TEXT NOT NULL,
+
+    CONSTRAINT "Dishes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OpeningHours" (
+    "id" TEXT NOT NULL,
+    "day" "DAYS" NOT NULL,
+    "openingHours" TEXT NOT NULL,
+    "closingHours" TEXT NOT NULL,
+    "RestaurantId" TEXT NOT NULL,
+
+    CONSTRAINT "OpeningHours_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Dishes" ADD CONSTRAINT "Dishes_RestaurantId_fkey" FOREIGN KEY ("RestaurantId") REFERENCES "Restaurants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OpeningHours" ADD CONSTRAINT "OpeningHours_RestaurantId_fkey" FOREIGN KEY ("RestaurantId") REFERENCES "Restaurants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
